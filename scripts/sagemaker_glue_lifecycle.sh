39f4b25a494dd4b51bf5d62fb601491dd26b9cce
Eric Bomarsi <ebomarsi@gmail.com>
2021-12-28 19:27:13 -0500

    parameterize source for maintenance

--- scripts/sagemaker_glue_lifecycle.sh
+++ scripts/sagemaker_glue_lifecycle.sh
@@ -3,8 +3,17 @@
 ##
 #!/bin/bash
 set -ex
+
+# cloud formation stack prefix is used as a unique resource id
 stackPrefix=$1
-shift
+
+# s3 source path (notebooks and scripts)
+source_s3_path="$2"
+shift 2
+
+# s3 data path
+data_s3_path='s3://emr-workshops-us-west-2/glue_immersion_day'
+
 if ! [ -e /home/ec2-user/glue_ready ]; then
 
     mkdir -p /home/ec2-user/glue
@@ -64,8 +73,8 @@ if ! [ -e /home/ec2-user/glue_ready ]; then
         sleep 20
         ((timeout -= 20))
         if [[ $timeout -lt 0 ]]; then
-            echo "ERROR: $LIVY_SERVER is unreachable"
-            exit 1
+            echo "WARNING: $LIVY_SERVER is unreachable"
+            break;
         fi
     done
     set -e
@@ -78,21 +87,23 @@ if ! [ -e /home/ec2-user/glue_ready ]; then
 fi
 
 # Download and save stack-info python script
-aws s3 cp s3://emr-workshops-us-west-2/glue_immersion_day/scripts/sagemaker-glue-get-stack-info.py /home/ec2-user/scripts/
+aws s3 cp ${source_s3_path}/scripts/sagemaker-glue-get-stack-info.py /home/ec2-user/scripts/
 chmod +x /home/ec2-user/scripts/sagemaker-glue-get-stack-info.py
-python /home/ec2-user/scripts/sagemaker-glue-get-stack-info.py $stackPrefix
+python /home/ec2-user/scripts/sagemaker-glue-get-stack-info.py ${stackPrefix}
 
 # Copy sample notebooks
-aws s3 cp --recursive s3://emr-workshops-us-west-2/glue_immersion_day/notebooks/ /home/ec2-user/SageMaker
+aws s3 cp --recursive ${source_s3_path}/notebooks/ /home/ec2-user/SageMaker
 
+# Data bucket
 s3_bucket=`cat stack-info.json | jq -r .S3Bucket`
 echo "s3_bucket : $s3_bucket"
 
 ## Copy the data
-aws s3 cp --recursive s3://emr-workshops-us-west-2/glue_immersion_day/data/ s3://$s3_bucket/data/
+
+aws s3 cp --recursive ${data_s3_path}/data/ s3://$s3_bucket/data/
 
 ## Copy the scripts
-aws s3 cp --recursive s3://emr-workshops-us-west-2/glue_immersion_day/scripts/ s3://$s3_bucket/scripts/
+aws s3 cp --recursive ${source_s3_path}/scripts/ s3://$s3_bucket/scripts/
 
 ## Install Jupyter extensions
 pip install jupyter_contrib_nbextensions
@@ -109,7 +120,7 @@ pip install psycopg2
 source deactivate
 
 # Load the data into Aurora
-aws s3 cp s3://emr-workshops-us-west-2/glue_immersion_day/scripts/salesdb.sql /home/ec2-user/scripts/salesdb.sql
+aws s3 cp ${data_s3_path}/scripts/salesdb.sql /home/ec2-user/scripts/salesdb.sql
 aurora_endpoint=`cat stack-info.json | jq -r .AuroraEndPoint.Address`
 echo "aurora_endpoint : $aurora_endpoint"
 mysql -f -u master -h $aurora_endpoint  --password="S3cretPwd99" < /home/ec2-user/scripts/salesdb.sql
@@ -118,7 +129,7 @@ mysql -f -u master -h $aurora_endpoint  --password="S3cretPwd99" < /home/ec2-use
 redshift_endpoint=`cat stack-info.json | jq -r .RedshiftEndPoint.Address`
 echo "redshift_endpoint : $redshift_endpoint"
 
-iam_role=$stackPrefix+'-GlueServiceRole'
+iam_role="${stackPrefix}-GlueServiceRole"
 
 ## Replace tokens in Notebook
 ## Module1
